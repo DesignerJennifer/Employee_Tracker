40901d9b7d13a75bec1dc27394910133620628f1
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-03-03 21:52:53 -0600

    mapping out all the functions I need to create

--- employeeTrackerApp.js
+++ employeeTrackerApp.js
@@ -1,11 +1,12 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table")
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port; if not 3306
+    // The port
     port: 3306,
 
     // Your username
@@ -13,6 +14,8 @@ var connection = mysql.createConnection({
 
     // Your password
     password: "4899",
+
+    // The database
     database: "employees_DB"
 });
 
@@ -23,134 +26,96 @@ connection.connect(function (err) {
     start();
 });
 
-// function which prompts the user for what action they should take
+// function: prompts the user for what action they should take
 function start() {
     inquirer
         .prompt({
-            name: "postOrBid",
+            name: "addToTracker",
             type: "list",
-            message: "Would you like to [POST] an auction or [BID] on an auction?",
-            choices: ["POST", "BID", "EXIT"]
+            message: "What do you want to do?",
+            choices: [
+                "View All Employees",
+                "View Employees by Department",
+                "View Employees by Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "View Departments",
+                "Add Department",
+                "Remove Department",
+                "View Roles",
+                "Add Role",
+                "Remove Role",
+                "EXIT"]
         })
         .then(function (answer) {
             // based on their answer, either call the bid or the post functions
-            if (answer.postOrBid === "POST") {
-                postAuction();
+            if (answer.addToTracker === "View All Employees") {
+                viewAllEmployees();
             }
-            else if (answer.postOrBid === "BID") {
-                bidAuction();
-            } else {
-                connection.end();
+            else if (answer.addToTracker === "View Employees by Department") {
+                viewEmployeesByDepartment()
             }
+            else if (answer.addToTracker === "View Employees by Manager") {
+                viewEmployeesByManger();
+            }
+            else if (answer.addToTracker === "Add Employee") {
+                addEmployee();
+            }
+            else if (answer.addToTracker === "Remove Employee") {
+                removeEmployee();
+            }
+            else if (answer.addToTracker === "Update Employee Role") {
+                updateEmployeeRole();
+            }
+            else if (answer.addToTracker === "Update Emploee Manager") {
+                updateEmployeeManager();
+            }
+            else if (answer.addToTracker === "Update Emploee Manager") {
+                updateEmployeeManager();
+            }
+
+
+        } else {
+            connection.end();
+        }
         });
 }
 
-// function to handle posting new items up for auction
-function postAuction() {
-    // prompt for info about the item being put up for auction
-    inquirer
-        .prompt([
-            {
-                name: "item",
-                type: "input",
-                message: "What is the item you would like to submit?"
-            },
-            {
-                name: "category",
-                type: "input",
-                message: "What category would you like to place your auction in?"
-            },
-            {
-                name: "startingBid",
-                type: "input",
-                message: "What would you like your starting bid to be?",
-                validate: function (value) {
-                    if (isNaN(value) === false) {
-                        return true;
-                    }
-                    return false;
-                }
-            }
-        ])
-        .then(function (answer) {
-            // when finished prompting, insert a new item into the db with that info
-            connection.query(
-                "INSERT INTO auctions SET ?",
-                {
-                    item_name: answer.item,
-                    category: answer.category,
-                    starting_bid: answer.startingBid || 0,
-                    highest_bid: answer.startingBid || 0
-                },
-                function (err) {
-                    if (err) throw err;
-                    console.log("Your auction was created successfully!");
-                    // re-prompt the user for if they want to bid or post
-                    start();
-                }
-            );
-        });
+// view all employees
+function viewAllEmployees() {
+    connection.query(`
+SELECT employee.id, employee.first_name, employee.last_name, role.title,
+concat_ws(' ',employee2.first_name , employee2.last_name) manager
+FROM employee   
+LEFT JOIN role ON employee.role_id=role.id
+LEFT JOIN employee employee2 ON  employee2.id=employee.manager_id;
+    `, function (err, data) {
+        console.table(data)
+        start()
+    })
 }
 
-function bidAuction() {
-    // query the database for all items being auctioned
-    connection.query("SELECT * FROM auctions", function (err, results) {
-        if (err) throw err;
-        // once you have the items, prompt the user for which they'd like to bid on
-        inquirer
-            .prompt([
-                {
-                    name: "choice",
-                    type: "rawlist",
-                    choices: function () {
-                        var choiceArray = [];
-                        for (var i = 0; i < results.length; i++) {
-                            choiceArray.push(results[i].item_name);
-                        }
-                        return choiceArray;
-                    },
-                    message: "What auction would you like to place a bid in?"
-                },
-                {
-                    name: "bid",
-                    type: "input",
-                    message: "How much would you like to bid?"
-                }
-            ])
-            .then(function (answer) {
-                // get the information of the chosen item
-                var chosenItem;
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].item_name === answer.choice) {
-                        chosenItem = results[i];
-                    }
-                }
+//view employees by department
+function viewEmployeesByDepartment() {
+    inquirer.prompt(
+        [
+            {
+                type: "input",
+                message: "What department do you want to add?",
+                name: "departmentName"
+            }
+        ]
+    ).then(function (input) {
+        var statement = connection.query("INSERT INTO department(dep_name) VALUES (?)", [input.departmentName], function (err) {
+            start()
+        })
+        console.log(statement.sql)
+    })
 
-                // determine if bid was high enough
-                if (chosenItem.highest_bid < parseInt(answer.bid)) {
-                    // bid was high enough, so update db, let the user know, and start over
-                    connection.query(
-                        "UPDATE auctions SET ? WHERE ?",
-                        [
-                            {
-                                highest_bid: answer.bid
-                            },
-                            {
-                                id: chosenItem.id
-                            }
-                        ],
-                        function (error) {
-                            if (error) throw err;
-                            console.log("Bid placed successfully!");
-                            start();
-                        }
-                    );
-                }
-                else {
-                    // bid wasn't high enough, so apologize and start over
-                    console.log("Your bid was too low. Try again...");
-                    start();
-                }
-            });
-    });
+}
+//view employees by manager
+function viewEmployeesByManger() {
+
 }
